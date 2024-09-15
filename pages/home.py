8462a3cb32cac72f4f5ebd1d83ee65d89fa0f1aa
Max <xsilva.max@gmail.com>
2024-09-15 16:19:35 -0300

    Commit do esboço

--- pages/home.py
+++ pages/home.py
@@ -1,4 +1,33 @@
 import flet as ft
+from components.components import CalcButton, CampoTxt, Headers, Alert
 
-def home():
-    return ft.Text(value='Home', color=ft.colors.WHITE, size=40)
+
+class ViewHome(ft.Container):
+
+    def __init__(self, page):
+
+        self.resultado = Alert()
+        self.salario = CampoTxt(texto='Salário bruto', prefix='R$ ')
+        self.jornada = CampoTxt(texto= 'Jornada mensal')
+        self.horas = CampoTxt(texto= 'Horas extras')
+        self.adicional = CampoTxt(texto= '% adicional', prefix='% ')
+
+        super().__init__(
+            content=ft.Column(
+                controls=[
+                    ft.Row(controls=[Headers(texto='Cálculo de hora extra', tipo='Título')]),
+                    ft.Row(controls=[Headers(texto='Cálculo de hora extra', tipo='Valor a recebe em extras ao final do mês')]),
+                    ft.Container(content=self.salario, margin=ft.margin.only(top=40)),
+                    ft.Container(content=self.jornada, margin=ft.margin.only(top=20)),
+                    ft.Container(content=self.horas, margin=ft.margin.only(top=20)),
+                    ft.Container(content=self.adicional, margin=ft.margin.only(top=20)),
+                    ft.Row(controls=[btn:=ft.Container(content=CalcButton(), margin=ft.margin.only(top=30))], alignment=ft.MainAxisAlignment.CENTER),
+                    ft.Row(controls=[ft.Container(content=self.resultado, margin=ft.margin.only(top=50))], alignment=ft.MainAxisAlignment.CENTER)
+                ],
+                spacing=0
+            ),
+            alignment=ft.alignment.center,
+            bgcolor=ft.colors.with_opacity(opacity=0.10, color=ft.colors.WHITE12),
+            padding=ft.padding.symmetric(vertical=10, horizontal=20),
+            height=page.height*0.88
+        )
--- pages/rescisao.py
+++
@@ -1,4 +0,0 @@
-import flet as ft
-
-def rescisao():
-    return ft.Text(value='Rescisão', color=ft.colors.WHITE, size=40)
